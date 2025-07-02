8cbbad22ca1ed695426cba7e86affec1de410051
Jessica Kiroff <jesskiroff@gmail.com>
2025-07-01 21:46:41 -0400

    add type conversion

--- data_types.py
+++ data_types.py
@@ -2,4 +2,22 @@
 
 #print("Hello"[-1]) #THIS WILL GIVE ME THE LAST VALUE OF THE STRING
 
-print(123_456)
+#print(123_456) all whole numbers whehter they r positive or neg are integers
+
+#FLOAT = floating pt number
+#print(3.14159)
+
+#Boolean 
+# print(True)
+# print(False)
+
+# len("hello")
+
+# print(type(123))
+
+# print(type("hi"))
+# print(type(123))
+# print(type(123.1))
+# print(type(False))
+
+print(int("123") + int("456"))
