fdfe8caf830455f7a497a504c025bd99d8f657aa
Jessica Kiroff <jesskiroff@gmail.com>
2025-07-02 23:16:42 -0400

    add remaining math operators

--- math_operators.py
+++ math_operators.py
@@ -3,3 +3,6 @@ print(123 + 456)
 print(7-3)
 print(3*2) #whenever u divide, even if your number can be divided evenly, the result will come up as a float (a decimal value)
 print(type(6/3))
+print(6//3) #known as floor division in python: it results in the integer pt of the division
+print(5%3) #modulo divison result 
+print (5**3) #Exponent form: 5 raised to the power of 3
