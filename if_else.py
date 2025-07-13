2b3379151de69a5ef6f5014ee1f25f8d56fc34f0
Jessica Kiroff <jesskiroff@gmail.com>
2025-07-12 21:58:25 -0400

    add if else statement

---
+++ if_else.py
@@ -0,0 +1,8 @@
+print ("Welcome to the rollercoaster! What ios your height? ")
+
+height = int(input("What is your height in cm? "))
+
+if height>= 120:
+    print("You can ride the rollercoaster! ")
+else:
+    print("You're too short kiddo, maybe next time!")
