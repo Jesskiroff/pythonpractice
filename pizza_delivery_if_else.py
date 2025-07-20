caa5ff930376129f5e939ea5ee105b87c0e3f363
Jessica Kiroff <jesskiroff@gmail.com>
2025-07-20 00:02:45 -0400

    fix uppercase issue

---
+++ pizza_delivery_if_else.py
@@ -0,0 +1,37 @@
+print("Welcome to python pizza ")
+size = input("What size pizza do you want? s, m, or l: ").upper()
+
+topping = input("Do you want pepperoni on your pizza? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese on your pizza? Y or N: ").upper()
+
+
+# topping_cost = 3
+# extra_cheese_cost = 2
+
+# if size == "s":
+#     price = 10
+#     print(f"The price is ${price}")
+# elif size == "m":
+#     price = 13
+#     print(f"The price is ${price}")
+# else:
+#     price = 15
+#     print(f"The price is ${price}")
+
+#     if
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill +=25
+
+if topping =="Y":
+    if size =="S":
+        bill += 2
+    else:
+        bill +=3
+
+print (bill)
