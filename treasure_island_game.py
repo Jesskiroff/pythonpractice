810ac084cb2571109a9764c8217bc608ec56eb8a
Jessica Kiroff <jesskiroff@gmail.com>
2025-07-23 21:25:08 -0400

    first attempt at treasure island if else game

--- treasure_island_game.py
+++ treasure_island_game.py
@@ -1,8 +1,24 @@
 print("Welcome to Treasure Island. Your mission is to find the treasure! ")
 
-which_door = input("Whcih door would you like to go in to: left or right. Input r or l.").upper()
+which_door = input("Which door would you like to go in to: left or right. Input r or l. ").lower()
 
-swim_or_wait_choice = input("Would you like to swim or wait? Input s or w. ").upper()
+swim_or_wait_choice = input("Would you like to swim or wait? Input s or w. ").lower()
 
-door_color_choice = input("Which of the doors do you want to go into: red, blue or yellow? Input r, b, or y ").upper()
+door_color_choice = input("Which of the doors do you want to go into: red, blue or yellow? Input r, b, or y ").lower()
 
+if which_door == "l":
+    print(swim_or_wait_choice)
+else: 
+    print("You got caught in a trap. You should have went through the left door. ")
+    if swim_or_wait_choice == "s":
+        print(door_color_choice)
+    else:
+        print("A shark ate you. You should have waited.")
+        if (door_color_choice) == "y":
+            print("Congradulations, you've found the treasure!")
+        else:
+            print("Sorry, you went through the wrong color door :(")
+    # else:
+    #     print("A shark ate you. You should have waited.")
+# else: 
+#     print("You got caught in a trap. You should have went through the left door. ")
