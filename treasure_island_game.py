e79cf330fa78f0a2b2eaff30936844d768b7cd08
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-07 10:35:19 -0400

    initialize if else statement alterating

--- treasure_island_game.py
+++ treasure_island_game.py
@@ -8,16 +8,24 @@ door_color_choice = input("Which of the doors do you want to go into: red, blue 
 
 if which_door == "l":
     print(swim_or_wait_choice)
+    if swim_or_wait_choice == "s":
+      print(door_color_choice)
+      if (door_color_choice) == "y":
+        print("Congradulations, you've found the treasure!")
+      else:
+        print("Sorry, you went through the wrong color door :(")
+    else:
+      print("A shark ate you. You should have waited.")
 else: 
     print("You got caught in a trap. You should have went through the left door. ")
-    if swim_or_wait_choice == "s":
-        print(door_color_choice)
-    else:
-        print("A shark ate you. You should have waited.")
-        if (door_color_choice) == "y":
-            print("Congradulations, you've found the treasure!")
-        else:
-            print("Sorry, you went through the wrong color door :(")
+# if swim_or_wait_choice == "s":
+#     print(door_color_choice)
+# else:
+#     print("A shark ate you. You should have waited.")
+# if (door_color_choice) == "y":
+#     print("Congradulations, you've found the treasure!")
+# else:
+#     print("Sorry, you went through the wrong color door :(")
     # else:
     #     print("A shark ate you. You should have waited.")
 # else: 
