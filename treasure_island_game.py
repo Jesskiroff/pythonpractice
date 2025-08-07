6d5738ed863f906ee3c4104083bf03af9995f6b4
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-07 10:57:09 -0400

    add working game

--- treasure_island_game.py
+++ treasure_island_game.py
@@ -1,15 +1,15 @@
 print("Welcome to Treasure Island. Your mission is to find the treasure! ")
 
-which_door = input("Which door would you like to go in to: left or right. Input r or l. ").lower()
+which_door = input("Which door would you like to go in to: left or right. Input r or l.\n").lower()
 
-swim_or_wait_choice = input("Would you like to swim or wait? Input s or w. ").lower()
+swim_or_wait_choice = input("Would you like to swim or wait? Input s or w.\n").lower()
 
-door_color_choice = input("Which of the doors do you want to go into: red, blue or yellow? Input r, b, or y ").lower()
+door_color_choice = input("Which of the doors do you want to go into: red, blue or yellow? Input r, b, or y\n").lower()
 
 if which_door == "l":
-    print(swim_or_wait_choice)
+    swim_or_wait_choice
     if swim_or_wait_choice == "s":
-      print(door_color_choice)
+      door_color_choice
       if (door_color_choice) == "y":
         print("Congradulations, you've found the treasure!")
       else:
