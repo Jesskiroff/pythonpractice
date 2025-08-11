467d7dfe1fec1d5fdd35b757a37dd84c7dea64d9
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-10 21:17:12 -0400

    experiment with random module

--- randomisation.py
+++ randomisation.py
@@ -7,3 +7,8 @@ import random
 
 random_number0_to_1 = random.random()
 print(random_number0_to_1)
+
+#0 inclusive nad up to 1 not included
+
+random_number1_to_10 = random.random() *10
+print(random_number1_to_10)
