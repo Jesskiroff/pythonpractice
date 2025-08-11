f6c40bd7649f7e03b53c607c8150343deab18c7c
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-10 21:48:44 -0400

    add random heads or tails

--- randomisation.py
+++ randomisation.py
@@ -1,14 +1,20 @@
-# import random
+import random
 
 # random_integer = random.randint(1,10)
 # print(random_integer)
 
-import random
-
-random_number0_to_1 = random.random()
-print(random_number0_to_1)
+# random_number0_to_1 = random.random()
+# print(random_number0_to_1)
 
 #0 inclusive nad up to 1 not included
 
-random_number1_to_10 = random.random() *10
-print(random_number1_to_10)
+# random_number1_to_10 = random.random() *10
+# print(random_number1_to_10)
+
+
+heads_or_tails = random.randint(0,1)
+
+if heads_or_tails == 0:
+    print("heads")
+else:
+    print ("tails")
