cebab8b54a4c559bfc533392c66001028a320e62
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-11 23:07:33 -0400

    add random list data structure practice

---
+++ list_and_random.py
@@ -0,0 +1,15 @@
+import random 
+
+friends = [ "Alice",
+    "Benjamin",
+    "Clara",
+    "David",
+    "Elena",
+    "Frank",
+    "Grace",
+    "Henry",
+    "Isabella",
+    "Jack"]
+
+who_pays_the_bill = random.choice(friends)
+print(who_pays_the_bill)
