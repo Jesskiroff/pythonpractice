3305c7211909e279249c47fdaca8075f0c13c924
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-12 11:34:00 -0400

    add nested list

---
+++ nested_list.py
@@ -0,0 +1,14 @@
+fruits_with_pesticides = ["strawberries", "nectarines"
+                          ,"apples"
+                          ,"grapes"
+                          ,"peaches",
+                          "cherries",
+                          "pears"]
+vegetables_with_pesticides = ["spinach",
+                              "kale",
+                              "tomatoes",
+                              "celery",
+                              "potatoes",]
+produce_with_pesticides = [fruits_with_pesticides, vegetables_with_pesticides]
+
+print(produce_with_pesticides)
