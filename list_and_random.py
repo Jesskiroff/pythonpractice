eed66fbf7a58740ba0d0ef22a5403377967e2460
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-12 11:00:40 -0400

    use len to work with list data structure

--- list_and_random.py
+++ list_and_random.py
@@ -13,3 +13,14 @@ friends = [ "Alice",
 
 who_pays_the_bill = random.choice(friends)
 print(who_pays_the_bill)
+
+
+#how to get the number value of items in a list
+print(len(friends))
+
+number_of_friends = len(friends)
+#if i try to print the last state by printing (friends(number_of_friends)), then I iwll get an 
+# error bc my last index would be 50 bc that's the length. 
+# the last index is always len- 1
+
+print(friends[number_of_friends - 1])
