3793a6fffee339c78ede1a2ffb2b05118d2dfe2e
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-21 21:24:51 -0400

    add for loops

---
+++ loops.py
@@ -0,0 +1,30 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+
+apples = ["Honeycrisp",
+"Granny Smith",
+"Fuji",
+"Gala",
+"Pink Lady",
+"Braeburn",
+"McIntosh"]
+
+for apple in apples:
+    print (apple) 
+
+apples = ["Honeycrisp",
+"Granny Smith",
+"Fuji",
+"Gala",
+"Pink Lady",
+"Braeburn",
+"McIntosh"]
+
+for apple in apples:
+    print (apples) #different than previous one bc here im printing out the entire list
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(f"{fruit} pie")
