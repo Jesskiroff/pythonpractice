8b4fb9796894e94fbe1e11a46b05a3d49b3ec515
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-21 21:54:05 -0400

    add find the average using a for loop

---
+++ math_loop.py
@@ -0,0 +1,13 @@
+test_scores = [88, 92, 76, 85, 90,
+67, 73, 81, 95, 78,
+84, 91, 69, 100, 58,
+87, 93, 72, 66, 80,
+77, 89, 94, 62, 71,
+]
+
+sum = 0
+for score in test_scores:
+    sum += score
+print(sum)
+number_of_scores = len(test_scores)
+print(sum/number_of_scores)
