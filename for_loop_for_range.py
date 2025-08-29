fa935b6baf1819f0c5ea294d5942d2e0dea27bb0
Jessica Kiroff <jesskiroff@gmail.com>
2025-08-29 18:54:25 -0400

    add gauss's value for range loop

--- for_loop_for_range.py
+++ for_loop_for_range.py
@@ -1,4 +1,13 @@
 #range func w for loop
 
-for number in range(1,10):
-    print(number)
+# for number in range(1,10):
+#     print(number)
+
+
+# for number in range(1,11,3):
+#     print (number)
+
+total = 0
+for number in range (1,101):
+    total+= number
+print(total)
