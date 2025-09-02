855c8ba33a13f5abc442fb2e837d59327b054d7e
Jessica Kiroff <jesskiroff@gmail.com>
2025-09-01 22:25:41 -0400

    add harder passowrd challenge

--- password_generator.py
+++ password_generator.py
@@ -8,15 +8,38 @@ numb_of_letters = int(input("How many letters would you like your password to ha
 numb_of_numbers = int(input("How many numbers would you like your password to have?\n"))
 numb_of_symbols = int(input("How many symbols would you like your password to have?\n"))
 
-for letter in range(1, numb_of_letters + 1):
-    pass_letters = random.choice(letters)
-    print(pass_letters)
+# password = ""
+
+# for letter in range(1, numb_of_letters + 1):
+#     pass_letters = random.choice(letters)
+#     password += pass_letters
+# # print(password)
 
 
-for number in range(1, numb_of_numbers + 1):
-    pass_numbers = random.choice(numbers)
-    print(pass_numbers)
+# for number in range(1, numb_of_numbers + 1):
+#     pass_numbers = random.choice(numbers)
+#     password += pass_numbers
+# # print(password)
 
-for symbol in range(1, numb_of_symbols +1):
-    pass_symbols = random.choice(symbols)
-    print(pass_symbols)
+# for symbol in range(1, numb_of_symbols +1):
+#     pass_symbols = random.choice(symbols)
+#     password += pass_symbols
+# print(password)
+
+password_list = []
+for character in range(0,numb_of_letters ):
+    password_list.append(random.choice(letters))
+
+for character in range(0,numb_of_numbers):
+    password_list.append(random.choice(numbers))
+
+for character in range(0,numb_of_symbols):
+    password_list.append(random.choice(symbols))
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for letter in password_list:
+    password+= letter
+print(password)
