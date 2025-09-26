df843a14568ff20f92d4e61b7e9583406e0e50e9
Jessica Kiroff <jesskiroff@gmail.com>
2025-09-25 21:02:34 -0400

    add functions

---
+++ funcs_w_param.py
@@ -0,0 +1,10 @@
+def greet():
+    print("Hello")
+    print("My name is Jessica")
+    print("What up what up?!")
+
+def greet_w_name(name, time_zone):
+    print(f"Hello {name}. Do you know that you're in the {time_zone} time zone now.")
+
+greet_w_name("Audrey", "Eastern")
+
