fe49a3a54a8113971ae05152af2ecc38fca73f44
Jessica Kiroff <jesskiroff@gmail.com>
2025-10-30 16:01:12 -0400

    add keep on bidding while loop into program

--- secret_auction_dictionary.py
+++ secret_auction_dictionary.py
@@ -1,7 +1,17 @@
-your_name = input("What is your name? ")
-bid_price = int(input("How much do you want to bid? "))
+def questions():
+    your_name = input("What is your name? ")
+    bid_price = int(input("How much do you want to bid? "))
 
-the_dictionary = {}
-the_dictionary[your_name] = [bid_price]
+    the_dictionary = {}
+    the_dictionary[your_name] = [bid_price]
 
-print(the_dictionary)
+    print(the_dictionary)
+
+keep_on_bidding = True
+other_bidder = input("is there another bidder? ").lower
+if other_bidder == "yes":
+    keep_on_bidding = True
+else:
+    keep_on_bidding = False
+while other_bidder:
+    questions()
