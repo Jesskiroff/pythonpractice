7364a85dcb91bcaeba78819134419c3292b15619
Jessica Kiroff <jesskiroff@gmail.com>
2025-10-30 16:46:45 -0400

    clean up auction program code.

--- sec_auc_dictpt2.py
+++ sec_auc_dictpt2.py
@@ -1,4 +1,7 @@
-name = input("What is your name? ")
-price =int(input("What is your bid?: $ "))
 bids= {}
-bids[name] = price
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name? ")
+    price =int(input("What is your bid?: $ "))
+    bids[name] = price
+    should_continue = input("Are there any other bidders?Type yes or no.\n")
