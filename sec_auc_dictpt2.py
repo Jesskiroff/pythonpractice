9c85df50fefd03ccdf8f07db72f8e2991c094b42
Jessica Kiroff <jesskiroff@gmail.com>
2025-10-30 16:40:36 -0400

    initialize second auction program

---
+++ sec_auc_dictpt2.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+price =int(input("What is your bid?: $ "))
+bids= {}
+bids[name] = price
--- secret_auction_dictionary.py
+++ secret_auction_dictionary.py
@@ -1,6 +1,7 @@
 def questions():
     your_name = input("What is your name? ")
     bid_price = int(input("How much do you want to bid? "))
+    print(other_bidder)
 
     the_dictionary = {}
     the_dictionary[your_name] = [bid_price]
