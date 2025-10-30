d15cfc0ed6c5b40b1f355dcf3f6e98ed1de4348a
Jessica Kiroff <jesskiroff@gmail.com>
2025-10-30 15:47:37 -0400

    initialize secret auction challenge

---
+++ secret_auction_dictionary.py
@@ -0,0 +1,7 @@
+your_name = input("What is your name? ")
+bid_price = int(input("How much do you want to bid? "))
+
+the_dictionary = {}
+the_dictionary[your_name] = [bid_price]
+
+print(the_dictionary)
