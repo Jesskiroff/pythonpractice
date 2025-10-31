9aef3ab842642126cb8132a33c5d8da3a9066488
Jessica Kiroff <jesskiroff@gmail.com>
2025-10-30 23:05:02 -0400

    add secret auction dictionary

--- secret_auction_dictionary.py
+++ secret_auction_dictionary.py
@@ -1,18 +1,37 @@
-def questions():
-    your_name = input("What is your name? ")
-    bid_price = int(input("How much do you want to bid? "))
-    print(other_bidder)
+# def questions():
+#     your_name = input("What is your name? ")
+#     bid_price = int(input("How much do you want to bid? "))
+#     print(other_bidder)
 
-    the_dictionary = {}
-    the_dictionary[your_name] = [bid_price]
+#     the_dictionary = {}
+#     the_dictionary[your_name] = [bid_price]
 
-    print(the_dictionary)
+#     print(the_dictionary)
 
-keep_on_bidding = True
-other_bidder = input("is there another bidder? ").lower
-if other_bidder == "yes":
-    keep_on_bidding = True
+# keep_on_bidding = True
+# other_bidder = input("is there another bidder? ").lower
+# if other_bidder == "yes":
+#     keep_on_bidding = True
+# else:
+#     keep_on_bidding = False
+# while other_bidder:
+#     questions()
+
+more_bidders = True
+
+name_of_bidder = input("What is your name?: ")
+bid_amount = int(input("How much would you like to bid?: $"))
+bid_continues = input("are there more bidders?").lower()
+
+while more_bidders == True:
+    
+if bid_continues == "yes":
+    continue
 else:
-    keep_on_bidding = False
-while other_bidder:
-    questions()
+    more_bidders= False
+
+
+dictionary_set = {}
+dictionary_set[name_of_bidder] = bid_amount
+
+print(dictionary_set)
