f03dfc4d6f101af1f6285525966ac30723de647c
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-04 13:44:13 -0500

    add function with format

--- format_func.py
+++ format_func.py
@@ -16,3 +16,15 @@ output_1 = function_2(function_1("hello"))
 output_2 = function_1(function_2("hello"))
 print(output_1)
 print(output_2)
+
+def format_name (first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "No name submitted"
+    formatted_first_name = first_name.title()
+    formatted_last_name = last_name.title()
+    return f"{formatted_first_name} {formatted_last_name}"
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+ 
+ 
