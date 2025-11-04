2ffd953340f0294adacbadf0ac94b405ae82f373
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-04 12:24:49 -0500

    add functions with other functions

--- format_func.py
+++ format_func.py
@@ -6,3 +6,13 @@ def format_name(f_name, l_name):
 format_name(f_name= "geOrge", l_name="BUSH")
 
 
+def function_1(text):
+    return text + text
+
+def function_2(text):
+    return text.title()
+
+output_1 = function_2(function_1("hello"))
+output_2 = function_1(function_2("hello"))
+print(output_1)
+print(output_2)
