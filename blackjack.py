5fb1d081af926510d327fb4c6fb6e96d7f8bc3ee
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-05 17:32:32 -0500

    add user's initial blackjack cards and computer's first card

--- blackjack.py
+++ blackjack.py
@@ -8,10 +8,7 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
-
-for _ in range (2):
-    user_cards.append(deal_card()) #when you want to add a single item to an existing list, you should use append instead of += (which is shorthand for the extend function)
-    computer_cards.append(deal_card())
+is_game_over = False
 
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
@@ -21,3 +18,14 @@ def calculate_score(cards):
         cards.append(11)
 
     return sum (cards)
+
+for _ in range (2):
+    user_cards.append(deal_card()) #when you want to add a single item to an existing list, you should use append instead of += (which is shorthand for the extend function)
+    computer_cards.append(deal_card())
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
