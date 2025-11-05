e9a61bd3801a9154406898aa8eee194d25940353
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-04 19:54:01 -0500

    add recursive function to finalize calculator

--- calculator.py
+++ calculator.py
@@ -17,15 +17,23 @@ operations["*"] = multiply
 operations["/"] = divide
 
 # print(operations["*"](n1=4, n2=8))
+def calculator():
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: +, -, *, or / ")
+        num2 = float(input("What is the next number?: "))
 
-should_accumulate = True
-num1 = float(input("What is the first number?: "))
-while should_accumulate:
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation: +, -, *, or / ")
-    num2 = float(input("What is the next number?: "))
+        answer = (operations[operation_symbol](num1, num2))
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to stop : ")
 
-    answer = (operations[operation_symbol](num1, num2))
-    print(f"{num1}{operation_symbol}{num2}{answer}")
-    
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+calculator()
