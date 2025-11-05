c2210bdc74323375d8a71b9bae50569540452b48
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-05 16:30:40 -0500

    add in append funtion to append card to user_cards list

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,13 @@
 import random
 
 def deal_card():
+    '''Returns a random card from the deck'''
     cards = [11, 2, 3, 4, 5, 6,7, 8, 9, 10,10,10,10]
     card = random.choice(cards)
     return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range (2):
+    user_cards.append(deal_card()) #when you want to add a single item to an existing list, you should use append instead of += (which is shorthand for the extend function)
