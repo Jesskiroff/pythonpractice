76b4a450c3f919e01e0dfa1d112489796ba39333
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-06 20:18:14 -0500

    add if else win or lose and reason why statements

--- blackjack.py
+++ blackjack.py
@@ -8,9 +8,12 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
+computer_score = -1
+user_score = -1
 is_game_over = False
 
 def calculate_score(cards):
+    '''Take a list of cards and return the score calculated from the list of cards'''
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
@@ -19,13 +22,40 @@ def calculate_score(cards):
 
     return sum (cards)
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "User has lost, opponent has a blackjack"
+    elif user_score == 0:
+        return "You win with a Blackjack!"
+    elif user_score > 21:
+        return "You went over, you lose"
+    elif computer_score > 21:
+        return "you win, computer score went over 21"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
 for _ in range (2):
     user_cards.append(deal_card()) #when you want to add a single item to an existing list, you should use append instead of += (which is shorthand for the extend function)
     computer_cards.append(deal_card())
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first card: {computer_cards[0]}")
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+
+while not is_game_over:
+        
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else: 
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
