9cf85e8f00885329b62111123de4306c784d96cb
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-10 20:03:32 -0500

    add in numbers game before hints

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,5 +1,6 @@
 import random 
-list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+
+list_of_numbers = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 
                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -10,6 +11,23 @@ list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
                    91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 
-print("Welcome to the number guessing game! \n I'm thinking of a number between 1 and 100.")
-print(input("Choose a difficulty. Type 'easy' or 'hard' : "))
+# print("Welcome to the number guessing game! \n I'm thinking of a number between 1 and 100.")
+difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard' : ") 
 
+number_chosen = random.choice(list_of_numbers) #computer automatically generates a number to be guessed by the user
+
+
+number_of_attempts = 0
+if difficulty_level == "hard":
+    number_of_attempts = 5
+else:
+    number_of_attempts = 10
+print(number_of_attempts)
+
+for x in range(1, number_of_attempts + 1):
+    answer_attempt = input (f"Guess a number. You have {number_of_attempts} guesses.")
+    number_of_attempts -= 1
+    if answer_attempt == number_chosen:
+        print(f"You guessed it! the answer is {answer_attempt}")
+    else:
+        print(f"Nope, you have {number_of_attempts} guesses left")
